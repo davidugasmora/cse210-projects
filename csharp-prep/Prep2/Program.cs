154c787915d81fd5e3134291bbc555b259899908
David Ugas Mora <uga21001@byui.edu>
2025-04-26 18:40:56 -0600

    Completing Assignments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int gradePercentage = int.Parse(userInput);
+
+        string letter = "";
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+
+        else if (gradePercentage < 60)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: '{letter}'");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("You passed, great job!");
+        }
+        else if (gradePercentage < 70)
+        {
+            Console.WriteLine("You didn't pass, better luck next time!");
+        }
     }
 }
