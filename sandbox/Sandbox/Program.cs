ee8ce9618f192cd1b8c212a3cd9e9931039fbe9e
David Ugas Mora <uga21001@byui.edu>
2025-04-29 09:35:39 -0600

    Some practice done.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+
+        int x = 10;
+        if (x == 10)
+        {
+            Console.WriteLine("X is 10");
+        }
+
+        for (int i = 0; i < x; i++)
+        {
+            Console.WriteLine($"Bob is cool: {i}");
+        }
+        
     }
 }
