e4d687532faa07d84999b2a3de11a6a68a08fa38
David Ugas Mora <uga21001@byui.edu>
2025-05-10 14:58:31 -0600

    prac

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,39 +1,16 @@
 using System;
 
-class Circle
-{
-    private double _radius;
-
-    public void SetRadius(double radius)
-    {
-        if (radius < 0 )
-        {
-            Console.WriteLine("Error");
-            return;
-        }
-
-        _radius = radius;
-    }
-
-    public double GetRadius()
-    {
-        return _radius;
-    }
-
-    public double GetArea()
-    {
-        return Math.PI * _radius * _radius;
-    }
-}
-
 class Program
 {    static void Main(string[] args)
     {
-
         Circle myCircle = new Circle();
         myCircle.SetRadius(10);
         Console.WriteLine($"{myCircle.GetRadius()}");
         Console.WriteLine($"{myCircle.GetArea()}");
 
+        Cylinder myCylinder = new Cylinder();
+        myCylinder.SetCircle(myCircle);
+        myCylinder.SetHeight(10);
+        Console.WriteLine($"{myCylinder.GetVolume()}");
     }
 }
